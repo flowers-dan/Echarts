686e90f1618555edfcabc9f2b90fc5fddf3a1fb2
916404323@qq.com <916404323@qq.com>
2020-07-05 12:54:01 +0800

    Initial commit

---
+++ EchartsProject/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EchartsprojectConfig(AppConfig):
+    name = 'EchartsProject'
---
+++ EchartsProject/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import json
+from django.http.response import JsonResponse
+import pymysql
+
+# Create your views here.
+def
